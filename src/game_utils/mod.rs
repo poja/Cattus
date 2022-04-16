8093728f45f37d13d2ec60b9330d12849ac2b1c0
Barak Ugav <barakugav@gmail.com>
2022-04-16 11:23:30 +0300

    Training: self play logic and saving into json

--- src/game_utils/mod.rs
+++ src/game_utils/mod.rs
@@ -1,2 +1,3 @@
 pub mod game;
 pub mod mcts;
+pub mod train;
---
+++ src/game_utils/train.rs
@@ -0,0 +1,115 @@
+use crate::game_utils::game::{GameColor, GamePosition, IGame};
+use crate::game_utils::mcts::MCTSPlayer;
+use json;
+use std::fs;
+use std::path;
+
+pub trait Encoder<Game: IGame> {
+    fn encode_moves(&self, moves: &Vec<(Game::Move, f32)>) -> Vec<f32>;
+    fn decode_moves(&self, moves: &Vec<f32>) -> Vec<(Game::Move, f32)>;
+    fn encode_position(&self, position: &Game::Position) -> Vec<f32>;
+}
+
+pub struct TrainData {
+    pos: Vec<f32>,
+    moves_probabilities: Vec<f32>,
+    winner: i8,
+}
+
+impl TrainData {
+    pub fn new(pos: Vec<f32>, moves_probabilities: Vec<f32>, winner: i8) -> Self {
+        Self {
+            pos: pos,
+            moves_probabilities: moves_probabilities,
+            winner: winner,
+        }
+    }
+}
+
+pub struct Trainer<'a, Game: IGame> {
+    encoder: &'a dyn Encoder<Game>,
+}
+
+impl<'a, Game: IGame> Trainer<'a, Game> {
+    pub fn new(encoder: &'a dyn Encoder<Game>) -> Self {
+        Self { encoder: encoder }
+    }
+
+    fn generate_data(
+        &self,
+        player: &mut MCTSPlayer<Game>,
+        games_num: u32,
+        output_dir: &String,
+    ) -> std::io::Result<()> {
+        fs::create_dir(output_dir)?;
+        let is_empty = path::PathBuf::from(output_dir).read_dir()?.next().is_none();
+        if !is_empty {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "output dir is not empty",
+            ));
+        }
+        let mut data_idx: u64 = 0;
+
+        for _ in 0..games_num {
+            let mut pos = Game::Position::new();
+            let mut pos_move_probs_pairs: Vec<(Game::Position, Vec<(Game::Move, f32)>)> =
+                Vec::new();
+
+            while !pos.is_over() {
+                /* Generate probabilities from MCTS player */
+                let moves = player.calc_moves_probabilities(&pos);
+                let m = player.choose_move_from_probabilities(&moves);
+
+                /* Store probabilities */
+                pos_move_probs_pairs.push((pos.clone(), moves));
+
+                /* Advance game position */
+                pos = match m {
+                    None => {
+                        eprintln!("player failed to choose a move");
+                        break;
+                    }
+                    Some(next_move) => pos.get_moved_position(next_move),
+                }
+            }
+            let winner = pos.get_winner();
+
+            for pos_move_probs_pair in pos_move_probs_pairs {
+                let data = TrainData::new(
+                    self.encoder.encode_position(&pos_move_probs_pair.0),
+                    self.encoder.encode_moves(&pos_move_probs_pair.1),
+                    match winner {
+                        None => 0,
+                        Some(winning_player) => match winning_player {
+                            GameColor::Player1 => 1,
+                            GameColor::Player2 => -1,
+                        },
+                    },
+                );
+
+                let filename = format!("d{:#016x}", data_idx);
+                self.write_data_to_file(data, filename)?;
+                data_idx += 1;
+            }
+        }
+
+        return Ok(());
+    }
+
+    fn write_data_to_file(&self, data: TrainData, filename: String) -> std::io::Result<()> {
+        let json_obj = json::object! {
+            position: data.pos,
+            moves_probabilities: data.moves_probabilities,
+            winner: data.winner
+        };
+        let json_str = json_obj.dump();
+        fs::write(filename, json_str)?;
+        return Ok(());
+    }
+
+    fn train_on_data_from_dir(&self, data_dirname: String) -> std::io::Result<()> {
+        return Ok(());
+    }
+
+}
