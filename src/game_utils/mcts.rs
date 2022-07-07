94ebec9f6846bcefdc393db7f9aa3f6a13e4d02f
Barak Ugav <barakugav@gmail.com>
2022-07-07 22:27:14 +0300

    MCTS: refactor selection and expantion
    Expand all, simulate one

--- src/game_utils/mcts.rs
+++ src/game_utils/mcts.rs
@@ -2,7 +2,7 @@ use petgraph::graph::{DiGraph, NodeIndex};
 use petgraph::visit::EdgeRef;
 use rand::prelude::IteratorRandom;
 
-use crate::game_utils::game::{GameColor, GamePlayer, GamePosition, IGame};
+use crate::game_utils::game::{GamePlayer, GamePosition, IGame};
 use crate::hex::simple_players::PlayerRand;
 
 /// Monte Carlo Tree Search (MCTS) implementation
@@ -19,16 +19,18 @@ struct MCTSNode<Position: GamePosition> {
 }
 
 impl<Position: GamePosition> MCTSNode<Position> {
-    pub fn from_position(pos: &Position) -> Self {
+    pub fn from_position(pos: Position) -> Self {
         Self {
-            position: *pos,
+            position: pos,
             simulations_n: 0,
             score_w: 0.0,
         }
     }
 
     pub fn get_expected_reward(&self) -> f32 {
-        assert!(self.simulations_n > 0);
+        if self.simulations_n == 0 {
+            return self.score_w;
+        }
         return (self.score_w as f32) / (self.simulations_n as f32);
     }
 }
@@ -54,13 +56,23 @@ impl<Game: IGame> MCTSPlayer<Game> {
 
     fn develop_tree(&mut self, root_id: NodeIndex<u32>) -> () {
         for _ in 1..self.simulations_per_move {
-            self.select_and_simulate(root_id);
+            /* Select a leaf node */
+            let path_to_selection = self.select(root_id);
+
+            /* Expand leaf */
+            let leaf_id: NodeIndex = *path_to_selection.last().unwrap();
+            self.create_children(leaf_id);
+
+            /* Simulate a single time from leaf */
+            let score = self.simulate(&path_to_selection);
+
+            /* back propagate the score to the parents */
+            self.backpropagate(&path_to_selection, score)
         }
     }
 
-    // Select a node to run simulation from (and add it to the search tree), return (leaf_ID, [nodes on path]).
-    // The list of nodes on path is used for back propagation.
-    fn select_and_simulate(&mut self, root_id: NodeIndex<u32>) {
+    /* Return path to selected leaf node */
+    fn select(&self, root_id: NodeIndex<u32>) -> Vec<NodeIndex<u32>> {
         let mut path: Vec<NodeIndex<u32>> = vec![];
 
         let mut node_id = root_id;
@@ -68,55 +80,12 @@ impl<Game: IGame> MCTSPlayer<Game> {
             path.push(node_id);
             let node = self.search_tree.node_weight(node_id).unwrap();
 
-            /* Node has no children, perform a (very) short simulation and done */
-            if node.position.is_over() {
-                let (player1_wins, player2_wins, draws) = match node.position.get_winner() {
-                    Some(color) => match color {
-                        GameColor::Player1 => (1, 0, 0),
-                        GameColor::Player2 => (0, 1, 0),
-                    },
-                    None => (0, 0, 1),
-                };
-                self.backpropagate(&path, player1_wins, player2_wins, draws);
-                return;
+            /* Node is leaf, done */
+            if node.position.is_over() || self.search_tree.edges(node_id).next().is_none() {
+                return path;
             }
 
-            // Node has no children in tree, expand all and simulate one simulation per child
-            if self.search_tree.edges(node_id).next().is_none() {
-                let parent_pos = node.position;
-
-                let (mut player1_wins, mut player2_wins, mut draws) = (0, 0, 0);
-                for m in parent_pos.get_legal_moves() {
-                    let leaf_pos = parent_pos.get_moved_position(m);
-                    let leaf_id = self
-                        .search_tree
-                        .add_node(MCTSNode::from_position(&leaf_pos));
-                    self.search_tree.add_edge(node_id, leaf_id, m);
-                    let mut leaf = self.search_tree.node_weight_mut(leaf_id).unwrap();
-                    match MCTSPlayer::<Game>::simulate_playout(&leaf_pos) {
-                        Some(color) => {
-                            if parent_pos.get_turn() == color {
-                                leaf.score_w += 1.0;
-                            }
-                            match color {
-                                GameColor::Player1 => player1_wins += 1,
-                                GameColor::Player2 => player2_wins += 1,
-                            };
-                        }
-                        None => {
-                            leaf.score_w += 0.5;
-                            draws += 1;
-                        }
-                    }
-                    leaf.simulations_n += 1;
-                }
-
-                /* back propagate a single time for all children */
-                self.backpropagate(&path, player1_wins, player2_wins, draws);
-                return;
-            }
-
-            /* Node children already been expanded, choose best one and continue in it's sub tree */
+            /* Node is not a leaf, choose best child and continue in it's sub tree */
             node_id = self
                 .search_tree
                 .edges(node_id)
@@ -137,39 +106,74 @@ impl<Game: IGame> MCTSPlayer<Game> {
         parent: &MCTSNode<Game::Position>,
         child: &MCTSNode<Game::Position>,
     ) -> f32 {
+        if child.simulations_n == 0 {
+            return f32::MAX;
+        }
         let exploit = (child.score_w as f32) / (child.simulations_n as f32);
         let explore = self.exploration_param_c
             * ((parent.simulations_n as f32).ln() / (child.simulations_n as f32)).sqrt();
         return exploit + explore;
     }
 
-    fn simulate_playout(pos: &Game::Position) -> Option<GameColor> {
-        // Play randomly and return the simulation game result
-        let mut player1 = PlayerRand::new();
-        let mut player2 = PlayerRand::new();
-        Game::play_until_over(pos, &mut player1, &mut player2).1
+    fn create_children(&mut self, parent_id: NodeIndex<u32>) {
+        let parent = self.search_tree.node_weight(parent_id).unwrap();
+        if parent.position.is_over() {
+            return;
+        }
+        let parent_pos = parent.position;
+        for m in parent.position.get_legal_moves() {
+            let leaf_pos = parent_pos.get_moved_position(m);
+            let leaf_id = self
+                .search_tree
+                .add_node(MCTSNode::from_position(leaf_pos));
+            self.search_tree.add_edge(parent_id, leaf_id, m);
+        }
     }
 
-    fn backpropagate(
-        &mut self,
-        path: &Vec<NodeIndex<u32>>,
-        player1_wins: u32,
-        player2_wins: u32,
-        draws: u32,
-    ) {
-        let first_node = self.search_tree.node_weight(path[0]).unwrap();
-        let games_num = player1_wins + player2_wins + draws;
-        let mut applied_score = (draws as f32) / 2.0
-            + match first_node.position.get_turn() {
-                GameColor::Player1 => player2_wins,
-                GameColor::Player2 => player1_wins,
-            } as f32;
+    fn simulate(&self, path_to_selection: &Vec<NodeIndex<u32>>) -> f32 {
+        let node_id: NodeIndex = *path_to_selection.last().unwrap();
+        let node = self.search_tree.node_weight(node_id).unwrap();
 
+        let score = MCTSPlayer::<Game>::simulate_playout(&node.position);
+
+        let root_id: NodeIndex = *path_to_selection.last().unwrap();
+        let root = self.search_tree.node_weight(root_id).unwrap();
+        if root.position.get_turn() == node.position.get_turn() {
+            return score;
+        } else {
+            return 1.0 - score;
+        }
+    }
+
+    fn simulate_playout(pos: &Game::Position) -> f32 {
+        let winner;
+        if pos.is_over() {
+            winner = pos.get_winner();
+        } else {
+            // Play randomly and return the simulation game result
+            let mut player1 = PlayerRand::new();
+            let mut player2 = PlayerRand::new();
+            winner = Game::play_until_over(pos, &mut player1, &mut player2).1
+        }
+        return match winner {
+            Some(color) => {
+                if color == pos.get_turn() {
+                    1.0
+                } else {
+                    0.0
+                }
+            }
+            None => 0.5,
+        };
+    }
+
+    fn backpropagate(&mut self, path: &Vec<NodeIndex<u32>>, score: f32) {
+        let mut applied_score = score;
         for node_id in path {
             let mut node = self.search_tree.node_weight_mut(*node_id).unwrap();
-            node.simulations_n += games_num;
+            node.simulations_n += 1;
             node.score_w += applied_score as f32;
-            applied_score = games_num as f32 - applied_score;
+            applied_score = 1.0 - applied_score;
         }
     }
 
@@ -179,7 +183,7 @@ impl<Game: IGame> MCTSPlayer<Game> {
     ) -> Vec<(Game::Move, f32)> {
         // Init search tree with one root node
         assert!(self.search_tree.node_count() == 0);
-        let root = MCTSNode::from_position(position);
+        let root = MCTSNode::from_position(*position);
         let root_id = self.search_tree.add_node(root);
 
         // Develop tree
