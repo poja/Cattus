52943d1bc4cc78bb601bdd34e2d75ed33d1f10ce
Barak Ugav <barakugav@gmail.com>
2022-08-21 17:24:11 +0300

    TicTacToe: formatting

--- src/bin/tictactoe_play_rand.rs
+++ src/bin/tictactoe_play_rand.rs
@@ -6,8 +6,8 @@ fn main() {
     let position = TicTacToePosition::new();
     let mut player1 = PlayerRand::new();
     let mut player2 = PlayerRand::new();
-    let game_result = TicTacToeGame::play_until_over(&position, &mut player1, &mut player2);
-    println!("The winner is: {}, details below:", color_to_str(game_result.1));
-    game_result.0.print();
+    let (final_pos, winner) = TicTacToeGame::play_until_over(&position, &mut player1, &mut player2);
+    println!("The winner is: {}, details below:", color_to_str(winner));
+    final_pos.print();
 
 }
