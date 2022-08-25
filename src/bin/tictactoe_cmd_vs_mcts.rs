df11d960ecc73ffa4bbfc139751ec26860b84708
Barak Ugav <barakugav@gmail.com>
2022-08-25 14:20:17 +0300

    Game: change game interface, for more complex games
    In chess, more information need to be stored in the game
    object, rather than the position object.

--- src/bin/tictactoe_cmd_vs_mcts.rs
+++ src/bin/tictactoe_cmd_vs_mcts.rs
@@ -1,9 +1,9 @@
 use clap::Parser;
-use rl::tictactoe::tictactoe_game::{TicTacToeGame, TicTacToePosition, color_to_str};
-use rl::game::common::{GamePosition, IGame};
+use rl::game::common::IGame;
+use rl::game::mcts::MCTSPlayer;
 use rl::tictactoe::cmd_player::TttPlayerCmd;
 use rl::tictactoe::net::two_headed_net::TwoHeadedNet;
-use rl::game::mcts::MCTSPlayer;
+use rl::tictactoe::tictactoe_game::{color_to_str, TicTacToeGame};
 
 #[derive(Parser, Debug)]
 #[clap(about, long_about = None)]
@@ -14,14 +14,14 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let position = TicTacToePosition::new();
     let mut player2 = TttPlayerCmd::new();
 
     let mut value_func = TwoHeadedNet::new(args.model_path);
     let mut mcts_player = MCTSPlayer::new_custom(1000, 1.41421, &mut value_func);
 
-    let (final_pos, winner) = TicTacToeGame::play_until_over(&position, &mut mcts_player, &mut player2);
+    let mut game = TicTacToeGame::new();
+
+    let (final_pos, winner) = game.play_until_over(&mut mcts_player, &mut player2);
     println!("The winner is: {}, details below:", color_to_str(winner));
     final_pos.print();
-
 }
