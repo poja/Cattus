d1e22ec7225fa5b0ed1ebf416e991277f9fa050f
Yishai Gronich <yishai.gronich@gmail.com>
2022-09-03 09:55:41 +0300

    Code style fixes - main.py, and RandPlayer

--- src/game/common.rs
+++ src/game/common.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub enum GameColor {
     Player1,
@@ -40,4 +42,24 @@ pub trait GameMove: Clone + Copy + Eq + std::cmp::Eq + std::hash::Hash + std::fm
 
 pub trait GamePlayer<Game: IGame> {
     fn next_move(&mut self, position: &Game::Position) -> Option<Game::Move>;
+}
+
+
+pub struct PlayerRand {}
+
+impl PlayerRand {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl<Game: IGame> GamePlayer<Game> for PlayerRand {
+    fn next_move(&mut self, position: &Game::Position) -> Option<Game::Move> {
+        let moves = position.get_legal_moves();
+        if moves.len() == 0 {
+            return None;
+        }
+        let mut rng = rand::thread_rng();
+        return Some(moves[rng.gen_range(0..moves.len()) as usize]);
+    }
 }
--- src/tictactoe/mod.rs
+++ src/tictactoe/mod.rs
@@ -1,4 +1,3 @@
 pub mod net;
 pub mod tictactoe_game;
 pub mod cmd_player;
-pub mod simple_players;
