43fb10c8a362c7e49e8d0ec8f3dbd07b9d7327b2
Barak Ugav <barakugav@gmail.com>
2022-09-18 14:56:48 +0300

    TwoHeadNet: use base class to share code accross games

---
+++ src/game/net.rs
@@ -0,0 +1,78 @@
+use tensorflow::{Graph, Operation, SavedModelBundle, SessionOptions, SessionRunArgs, Tensor};
+
+pub struct TwoHeadedNetBase {
+    bundle: SavedModelBundle,
+    input_op: Operation,
+    value_head: Operation,
+    policy_head: Operation,
+}
+
+impl TwoHeadedNetBase {
+    pub fn new(
+        model_path: &String,
+        input_name: &str,
+        value_head_name: &str,
+        policy_head_name: &str,
+    ) -> Self {
+        // Load saved model bundle (session state + meta_graph data)
+        let mut graph = Graph::new();
+        let bundle =
+            SavedModelBundle::load(&SessionOptions::new(), &["serve"], &mut graph, model_path)
+                .expect("Can't load saved model");
+
+        // Get signature metadata from the model bundle
+        let signature = bundle
+            .meta_graph_def()
+            .get_signature("serving_default")
+            .unwrap();
+
+        // Get input/output info
+        let input_info = signature.get_input(input_name).unwrap();
+        let output_scalar_info = signature.get_output(value_head_name).unwrap();
+        let output_probs_info = signature.get_output(policy_head_name).unwrap();
+
+        // Get input/output ops from graph
+        let input_op = graph
+            .operation_by_name_required(&input_info.name().name)
+            .unwrap();
+        let value_head = graph
+            .operation_by_name_required(&output_scalar_info.name().name)
+            .unwrap();
+        let policy_head = graph
+            .operation_by_name_required(&output_probs_info.name().name)
+            .unwrap();
+
+        Self {
+            bundle: bundle,
+            input_op: input_op,
+            value_head: value_head,
+            policy_head: policy_head,
+        }
+    }
+
+    pub fn run_net(&self, input: Tensor<f32>) -> (f32, Tensor<f32>) {
+        let mut args = SessionRunArgs::new();
+        args.add_feed(&self.input_op, 0, &input);
+        let output_scalar = args.request_fetch(&self.value_head, 1);
+        let output_probs = args.request_fetch(&self.policy_head, 0);
+
+        self.bundle
+            .session
+            .run(&mut args)
+            .expect("Error occurred during calculations");
+
+        let mut val: f32 = args.fetch(output_scalar).unwrap()[0];
+        if val.is_nan() {
+            val = 0.0;
+        }
+
+        let mut probs: Tensor<f32> = args.fetch(output_probs).unwrap();
+        for idx in 0..probs.len() {
+            if probs[idx].is_nan() {
+                probs[idx] = f32::MIN;
+            }
+        }
+
+        return (val, probs);
+    }
+}
--- src/hex/net/two_headed_net.rs
+++ src/hex/net/two_headed_net.rs
@@ -1,73 +1,22 @@
 use crate::game::common::{GamePosition, IGame};
 use crate::game::mcts::ValueFunction;
+use crate::game::net::TwoHeadedNetBase;
 use crate::hex::hex_game::{HexGame, HexPosition};
 use crate::hex::net::common;
 use crate::hex::net::encoder::Encoder;
 use itertools::Itertools;
-use tensorflow::{Graph, Operation, SavedModelBundle, SessionOptions, SessionRunArgs, Tensor};
 
 pub struct TwoHeadedNet {
-    bundle: SavedModelBundle,
+    base: TwoHeadedNetBase,
     encoder: Encoder,
-    input_op: Operation,
-    output_scalar_op: Operation,
-    output_probs_op: Operation,
 }
 
 impl TwoHeadedNet {
     pub fn new(model_path: &String) -> Self {
-        let signature_input_parameter_name = "in_position";
-        let signature_output_value_parameter_name = "out_value";
-        let signature_output_probs_parameter_name = "out_probs";
-
-        // Load saved model bundle (session state + meta_graph data)
-        let mut graph = Graph::new();
-        let bundle =
-            SavedModelBundle::load(&SessionOptions::new(), &["serve"], &mut graph, model_path)
-                .expect("Can't load saved model");
-
-        // Get signature metadata from the model bundle
-        let signature = bundle
-            .meta_graph_def()
-            .get_signature("serving_default")
-            .unwrap();
-
-        // Get input/output info
-        let input_info = signature.get_input(signature_input_parameter_name).unwrap();
-        let output_scalar_info = signature
-            .get_output(signature_output_value_parameter_name)
-            .unwrap();
-        let output_probs_info = signature
-            .get_output(signature_output_probs_parameter_name)
-            .unwrap();
-
-        // Get input/output ops from graph
-        let input_op = graph
-            .operation_by_name_required(&input_info.name().name)
-            .unwrap();
-        let output_scalar_op = graph
-            .operation_by_name_required(&output_scalar_info.name().name)
-            .unwrap();
-        let output_probs_op = graph
-            .operation_by_name_required(&output_probs_info.name().name)
-            .unwrap();
-
         Self {
-            bundle: bundle,
+            base: TwoHeadedNetBase::new(model_path, "in_position", "out_value", "out_probs"),
             encoder: Encoder::new(),
-            input_op: input_op,
-            output_scalar_op: output_scalar_op,
-            output_probs_op: output_probs_op,
         }
-    }
-
-    pub fn evaluate_position(
-        &self,
-        position: &HexPosition,
-    ) -> (f32, Vec<(<HexGame as IGame>::Move, f32)>) {
-        let (flipped_pos, is_flipped) = common::flip_pos_if_needed(*position);
-        let eval = self.evaluate_position_impl(&flipped_pos);
-        return common::flip_score_if_needed(eval, is_flipped);
     }
 
     fn evaluate_position_impl(
@@ -75,28 +24,8 @@ impl TwoHeadedNet {
         position: &HexPosition,
     ) -> (f32, Vec<(<HexGame as IGame>::Move, f32)>) {
         let input = self.encoder.encode_position(position);
+        let (val, probs) = self.base.run_net(input);
 
-        let mut args = SessionRunArgs::new();
-        args.add_feed(&self.input_op, 0, &input);
-        let output_scalar = args.request_fetch(&self.output_scalar_op, 1);
-        let output_probs = args.request_fetch(&self.output_probs_op, 0);
-
-        self.bundle
-            .session
-            .run(&mut args)
-            .expect("Error occurred during calculations");
-
-        let mut val: f32 = args.fetch(output_scalar).unwrap()[0];
-        if val.is_nan() {
-            val = 0.0;
-        }
-
-        let mut probs: Tensor<f32> = args.fetch(output_probs).unwrap();
-        for idx in 0..probs.len() {
-            if probs[idx].is_nan() {
-                probs[idx] = f32::MIN;
-            }
-        }
         let moves = position.get_legal_moves();
         let moves_probs = moves
             .iter()
@@ -109,6 +38,8 @@ impl TwoHeadedNet {
 
 impl ValueFunction<HexGame> for TwoHeadedNet {
     fn evaluate(&mut self, position: &HexPosition) -> (f32, Vec<(<HexGame as IGame>::Move, f32)>) {
-        return self.evaluate_position(position);
+        let (flipped_pos, is_flipped) = common::flip_pos_if_needed(*position);
+        let eval = self.evaluate_position_impl(&flipped_pos);
+        return common::flip_score_if_needed(eval, is_flipped);
     }
 }
