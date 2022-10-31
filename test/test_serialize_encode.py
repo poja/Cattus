b6b527ae9bc554d27f357b17e892824fc32f90fb
Barak Ugav <barakugav@gmail.com>
2022-10-31 13:03:50 +0200

    Tests: refactor serialize encode tests args

--- test/test_serialize_encode.py
+++ test/test_serialize_encode.py
@@ -21,17 +21,16 @@ TMP_DIR = os.path.join(TESTS_DIR, "tmp", "test_serialize_encode")
 SERIALIZE_FILE = os.path.join(TMP_DIR, "serialize_res.json")
 ENCODE_FILE = os.path.join(TMP_DIR, "encode_res.json")
 
-
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[Serialize Encode Test]: %(message)s')
+    format="[Serialize Encode Test]: %(message)s")
 
 
 def test_ttt_serialize_encode():
-    _test_serialize_encode({
-        "game_name": "tictactoe",
-        "game_obj": TicTacToe(),
-        "positions": [
+    _test_serialize_encode(
+        "tictactoe",
+        TicTacToe(),
+        [
             "___x__o_ox",
             "o_xx_x__ox",
             "o__xo__xxx",
@@ -39,14 +38,14 @@ def test_ttt_serialize_encode():
             "oo__x____x",
             "oo__o__oxx",
         ]
-    })
+    )
 
 
 def test_hex_serialize_encode():
-    _test_serialize_encode({
-        "game_name": "hex",
-        "game_obj": Hex(),
-        "positions": [
+    _test_serialize_encode(
+        "hex",
+        Hex(),
+        [
             "reeeeeeeeee"
             "eeeeeeeeeee"
             "eeeeeeeeeee"
@@ -84,25 +83,25 @@ def test_hex_serialize_encode():
             "eeeeeeeeeer"
             "r"
         ]
-    })
+    )
 
 
 def test_chess_serialize_encode():
-    _test_serialize_encode({
-        "game_name": "chess",
-        "game_obj": Chess(),
-        "positions": [
+    _test_serialize_encode(
+        "chess",
+        Chess(),
+        [
             "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
             "nnqrkbbr/pppppppp/8/8/8/8/PPPPPPPP/NNQRKBBR w - - 0 1",
             "8/1p6/3QR3/6k1/1P2b3/2P3K1/6b1/6r1/ w - - 0 1",
             "4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1"
-            # "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1" TODO
+            "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1"
         ]
-    })
+    )
 
 
-def _test_serialize_encode(args):
-    for position in args["positions"]:
+def _test_serialize_encode(game_name, game, positions):
+    for position in positions:
         if os.path.exists(TMP_DIR):
             shutil.rmtree(TMP_DIR)
         os.makedirs(TMP_DIR)
@@ -111,20 +110,19 @@ def _test_serialize_encode(args):
             subprocess.run([
                 "cargo", "run", "--profile", "dev", "-q", "--bin",
                 "test_encode", "--",
-                "--game", args["game_name"],
+                "--game", game_name,
                 "--position", position,
                 "--outfile", ENCODE_FILE],
                 stderr=sys.stderr, stdout=sys.stdout, check=True)
             subprocess.run([
                 "cargo", "run", "--profile", "dev", "-q", "--bin",
                 "test_serialize", "--",
-                "--game", args["game_name"],
+                "--game", game_name,
                 "--position", position,
                 "--outfile", SERIALIZE_FILE],
                 stderr=sys.stderr, stdout=sys.stdout, check=True)
 
             cpu = True
-            game = args["game_obj"]
             packed_entry = game.load_data_entry(SERIALIZE_FILE)
             nparr_entry = DataParser.unpack_planes(packed_entry, game, cpu)
             bytes_entry = DataParser.serialize(nparr_entry, game)
@@ -137,7 +135,7 @@ def _test_serialize_encode(args):
 
             planes_rust_shape = tuple(rust_tensor_data["shape"])
             if planes_rust_shape != planes_dpt.shape:
-                print("game", args["game_name"])
+                print("game", game_name)
                 print("position", position)
                 raise ValueError("planes tensor shape mismatch",
                                  planes_rust_shape, planes_dpt.shape)
@@ -148,8 +146,15 @@ def _test_serialize_encode(args):
             if (planes_rust != planes_dpt).any():
 
                 raise ValueError(f"Planes tensor mismatch."
-                                 f"(Game: {args['game_name']}, Position: {position}, "
+                                 f"(Game: {game_name}, Position: {position}, "
                                  f"Rust planes: {planes_rust}, Data parser planes: {planes_dpt})")
         finally:
             if REMOVE_TMP_DIR_ON_FINISH:
                 shutil.rmtree(TMP_DIR)
+
+
+if __name__ == "__main__":
+    test_ttt_serialize_encode()
+    test_hex_serialize_encode()
+    test_chess_serialize_encode()
+    logging.info("test passed")
