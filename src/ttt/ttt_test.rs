ee87f15e9d45a92d5959a6f179d0010459cf1a26
Barak Ugav <barakugav@gmail.com>
2022-11-15 10:35:25 +0200

    Tests: more flipping tests

--- src/ttt/ttt_test.rs
+++ src/ttt/ttt_test.rs
@@ -1,7 +1,11 @@
 #[cfg(test)]
 mod tests {
-    use crate::game::common::{GameColor, GamePosition};
-    use crate::ttt::ttt_game::TttPosition;
+    use rand::rngs::StdRng;
+    use rand::{Rng, RngCore, SeedableRng};
+    use std::collections::HashSet;
+
+    use crate::game::common::{GameColor, GameMove, GamePlayer, GamePosition, IGame, PlayerRand};
+    use crate::ttt::ttt_game::{TttGame, TttMove, TttPosition};
 
     #[test]
     fn simple_game_and_mate() {
@@ -32,6 +36,42 @@ mod tests {
         {
             assert!(pos.get_turn().opposite() == pos.get_flip().get_turn());
             assert!(pos.get_flip().get_flip() == pos);
+        }
+    }
+
+    #[test]
+    fn flip_rand() {
+        let seed: u64 = rand::thread_rng().gen();
+        println!("[{}] Using seed {}", stringify!(flip_rand), seed);
+        let mut rand = StdRng::seed_from_u64(seed);
+
+        let games_num = 100;
+        for _ in 0..games_num {
+            let mut player: Box<dyn GamePlayer<TttGame>> =
+                Box::new(PlayerRand::from_seed(rand.next_u64() ^ 0xe4655449311aee87));
+            let mut game = TttGame::new();
+
+            while !game.is_over() {
+                let pos = *game.get_position();
+                let pos_t = pos.get_flip();
+
+                /* Assert flip of flip is original */
+                assert!(pos == pos_t.get_flip());
+
+                /* Assert flip of moves of flip are original moves */
+                let moves: HashSet<TttMove> = HashSet::from_iter(pos.get_legal_moves());
+                let moves_tt: HashSet<TttMove> =
+                    HashSet::from_iter(pos_t.get_legal_moves().into_iter().map(|m| m.get_flip()));
+                assert!(moves == moves_tt);
+
+                /* Assert game result is the same */
+                assert!(pos.is_over() == pos_t.is_over());
+                if pos.is_over() {
+                    assert!(pos.get_winner() == pos_t.get_winner().map(|w| w.opposite()));
+                }
+
+                game.play_single_turn(player.next_move(&game.get_position()).unwrap());
+            }
         }
     }
 }
