4c883cb8016348b5571f9fc7a006781a716180c4
Barak Ugav <barakugav@gmail.com>
2022-11-15 09:53:47 +0200

    RandPlayer: support input seed

--- src/bin/hex_uxi_rand.rs
+++ src/bin/hex_uxi_rand.rs
@@ -1,7 +1,7 @@
 use cattus::{game::common::PlayerRand, hex::uxi};
 
 fn main() {
-    let player = Box::new(PlayerRand {});
+    let player = Box::new(PlayerRand::new());
     let mut engine = uxi::UXIEngine::new(player);
     engine.run();
 }
