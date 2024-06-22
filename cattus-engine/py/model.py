900d06f915010616d57a8cf9f9a9563f79c296be
Barak Ugav <barakugav@gmail.com>
2024-06-22 08:49:37 +0300

    Save model in ONNX format

--- cattus-engine/py/model.py
+++ cattus-engine/py/model.py
@@ -1,32 +1,21 @@
-import tensorflow as tf
-from cattus_train.net_utils import (
-    loss_cross_entropy,
-    policy_head_accuracy,
-    value_head_accuracy,
-)
+import numpy as np
+from onnxruntime import InferenceSession
 
 
 class Model:
     def __init__(self, model_path: str):
-        self._model = tf.keras.models.load_model(
-            model_path,
-            custom_objects={
-                "loss_cross_entropy": loss_cross_entropy,
-                "value_head_accuracy": value_head_accuracy,
-                "policy_head_accuracy": policy_head_accuracy,
-            },
-        )
+        self._model = InferenceSession(model_path)
 
     def call(self, x_shape: list[int], x_data: list[float]):
-        x = tf.reshape(tf.convert_to_tensor(x_data), x_shape)
+        x = np.array(x_data, dtype=np.float32).reshape(x_shape)
         n = x.shape[0]
 
-        outputs = self._model(x)
+        outputs = self._model.run(None, {"input_planes": x})
         assert len(outputs) == 2
         vals, probs = outputs[0], outputs[1]
 
         assert vals.shape == (n, 1)
         assert probs.shape[0] == n
-        vals = vals.numpy().reshape(n).tolist()
-        probs = probs.numpy().reshape(n, -1).tolist()
+        vals = vals.reshape(n).tolist()
+        probs = probs.reshape(n, -1).tolist()
         return vals, probs
