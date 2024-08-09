5125d183339331cc949591245cd66dad31a782d8
Barak Ugav <barakugav@gmail.com>
2024-08-09 13:11:39 +0300

    Rework batch model computation

--- cattus-engine/src/bin/test_net_output.rs
+++ cattus-engine/src/bin/test_net_output.rs
@@ -1,11 +1,13 @@
 use cattus::chess::chess_game::{ChessGame, ChessPosition};
-use cattus::game::net::{self, TwoHeadedNetBase};
+use cattus::game::model::Model;
+use cattus::game::net;
 use cattus::hex::hex_game::{HexGame, HexPosition};
 use cattus::ttt::ttt_game::{TttGame, TttPosition};
-use cattus::utils::{self, Device};
+use cattus::utils;
 use cattus::{chess, hex, ttt};
 use clap::Parser;
 use itertools::Itertools;
+use ndarray::{Array2, ArrayD};
 use std::fs;
 
 #[derive(Parser, Debug)]
@@ -41,40 +43,47 @@ fn main() -> std::io::Result<()> {
     outputs_to_json(outputs, &args.outfile)
 }
 
-fn run_net_tictactoe(args: &Args) -> Vec<(Vec<f32>, f32)> {
+fn run_net_tictactoe(args: &Args) -> Vec<ArrayD<f32>> {
     let pos = TttPosition::from_str(&args.position);
-    let net = TwoHeadedNetBase::<TttGame>::new(&args.model_path, Device::Cpu, None);
+    let model = Model::new(&args.model_path);
     let samples = (0..args.repeat)
         .map(|_| ttt::net::common::position_to_planes(&pos))
         .collect_vec();
     let tensor = net::planes_to_tensor::<TttGame>(&samples);
-    net.run_net(tensor)
+    model.run(vec![tensor.into_dyn()])
 }
 
-fn run_net_hex<const BOARD_SIZE: usize>(args: &Args) -> Vec<(Vec<f32>, f32)> {
+fn run_net_hex<const BOARD_SIZE: usize>(args: &Args) -> Vec<ArrayD<f32>> {
     let pos = HexPosition::from_str(&args.position);
-    let net = TwoHeadedNetBase::<HexGame<BOARD_SIZE>>::new(&args.model_path, Device::Cpu, None);
+    let model = Model::new(&args.model_path);
     let samples = (0..args.repeat)
         .map(|_| hex::net::common::position_to_planes(&pos))
         .collect_vec();
     let tensor = net::planes_to_tensor::<HexGame<BOARD_SIZE>>(&samples);
-    net.run_net(tensor)
+    model.run(vec![tensor.into_dyn()])
 }
 
-fn run_net_chess(args: &Args) -> Vec<(Vec<f32>, f32)> {
+fn run_net_chess(args: &Args) -> Vec<ArrayD<f32>> {
     let pos = ChessPosition::from_str(&args.position);
-
-    let net = TwoHeadedNetBase::<ChessGame>::new(&args.model_path, Device::Cpu, None);
+    let model = Model::new(&args.model_path);
     let samples = (0..args.repeat)
         .map(|_| chess::net::common::position_to_planes(&pos))
         .collect_vec();
     let tensor = net::planes_to_tensor::<ChessGame>(&samples);
-    net.run_net(tensor)
+    model.run(vec![tensor.into_dyn()])
 }
 
-fn outputs_to_json(outputs: Vec<(Vec<f32>, f32)>, filename: &String) -> std::io::Result<()> {
-    let vals = outputs.iter().map(|(_probs, val)| *val).collect_vec();
-    let probs = outputs.into_iter().map(|(probs, _val)| probs).collect_vec();
+fn outputs_to_json(mut outputs: Vec<ArrayD<f32>>, filename: &String) -> std::io::Result<()> {
+    assert_eq!(outputs.len(), 2);
+    let probs: Array2<f32> = outputs.remove(0).into_dimensionality().unwrap();
+    let vals: Array2<f32> = outputs.remove(0).into_dimensionality().unwrap();
+    let probs = probs
+        .rows()
+        .into_iter()
+        .map(|row| row.to_vec())
+        .collect_vec();
+    assert_eq!(vals.shape()[1], 1);
+    let vals = vals.into_raw_vec();
     fs::write(
         filename,
         json::object! {
