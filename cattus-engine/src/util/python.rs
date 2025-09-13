273c33d9448dd297e2f30027f5a8140e146ab32b
Barak Ugav <barakugav@gmail.com>
2025-09-13 15:30:23 +0300

    Add Unwrapy trait, printing python stack trace

---
+++ cattus-engine/src/util/python.rs
@@ -0,0 +1,15 @@
+use pyo3::prelude::*;
+
+pub(crate) trait Unwrapy<T> {
+    fn unwrapy(self, py: Python) -> T;
+}
+
+impl<T> Unwrapy<T> for PyResult<T> {
+    fn unwrapy(self, py: Python) -> T {
+        self.map_err(|err| {
+            err.print_and_set_sys_last_vars(py);
+            err
+        })
+        .unwrap()
+    }
+}
