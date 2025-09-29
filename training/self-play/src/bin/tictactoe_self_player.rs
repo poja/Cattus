16d9df5d1890a79950daaba90703d1db5ee6fcbf
Barak Ugav <barakugav@gmail.com>
2025-09-29 09:58:34 +0300

    Remove INNetworkBuilder, not needed

--- training/self-play/src/bin/tictactoe_self_player.rs
+++ training/self-play/src/bin/tictactoe_self_player.rs
@@ -1,26 +1,6 @@
-use cattus::mcts::cache::ValueFuncCache;
-use cattus::mcts::value_func::ValueFunction;
-use cattus::net::model::InferenceConfig;
-use cattus::net::NNetwork;
-use cattus::ttt::TttGame;
-use cattus_self_play::self_play_cmd::{run_main, INNetworkBuilder};
+use cattus_self_play::self_play_cmd::run_main;
 use cattus_self_play::serialize::ttt::TttSerializer;
-use std::path::Path;
-use std::sync::Arc;
-
-struct NNetworkBuilder;
-impl INNetworkBuilder<TttGame> for NNetworkBuilder {
-    fn build_net(
-        &self,
-        model_path: &Path,
-        inference_cfg: InferenceConfig,
-        batch_size: usize,
-        cache: Arc<ValueFuncCache<TttGame>>,
-    ) -> Box<dyn ValueFunction<TttGame>> {
-        Box::new(NNetwork::new(model_path, inference_cfg, batch_size, Some(cache)))
-    }
-}
 
 fn main() -> std::io::Result<()> {
-    run_main(Box::new(NNetworkBuilder), Box::new(TttSerializer))
+    run_main(Box::new(TttSerializer))
 }
--- training/self-play/src/lib.rs
+++ training/self-play/src/lib.rs
@@ -1,4 +1,3 @@
-pub mod hex_self_player;
 pub mod self_play;
 pub mod self_play_cmd;
 pub mod serialize;
